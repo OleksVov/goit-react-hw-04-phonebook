88bcdebcf9cf56fcfce4bd47dee0bfce3a7cf12e
Oleksandr <alex.vovchok@gmail.com>
2023-05-01 22:04:34 +0300

    refactorin contactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,43 +1,64 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import PropTypes from 'prop-types';
 import css from './ContactForm.module.css'
 
 
-class ContactForm extends Component{
-state = {
-    name: '',
-    number: '',
+function ContactForm({onSubmit}) {
+
+    const[name, setName] = useState('');
+    const[number, setNumber] = useState('');
+
+// const handleNameChange = event => {
+//     setName(event.target.value);
+// };
+
+// const handleNumberChange = event => {
+//     setNumber(event.target.value);
+// };
+
+const handleChange = event => {
+    const { name, value } = event.target;
+
+    switch (name) {
+        case 'name':
+            setName(value);
+            break;
+
+            case 'number':
+            setNumber(value);
+            break;
+    
+        default:
+            console.warm('This type of field is not processed');
+    }
 };
 
-handleChange = event => {
-    const {name, value} = event.target;
-    this.setState({
-        [name]:value})
-  };
-
-handleSubmit = e => {
+const handleSubmit = e => {
     e.preventDefault();
 
-    this.props.onSubmit(this.state.name, this.state.number);
-    this.reset();
+    onSubmit(name, number);
+    resetName();
+    resetNumber();
 }
 
-reset = () => {
-    this.setState({name: '', number: ''})
-  }
+const resetName = () => {
+    setName('')
+  };
 
-    render() {
+  const resetNumber = () => {
+    setNumber('')
+  };
 
-        const{name, number} = this.state
+    
         return (
-            <form className={css.form} onSubmit={this.handleSubmit}>
+            <form className={css.form} onSubmit={handleSubmit}>
             <label className={css.label}>
             Name
              <input
          type="text"
          name="name"
          value={name}
-         onChange={this.handleChange}
+         onChange={handleChange}
          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
          required
@@ -50,7 +71,7 @@ reset = () => {
         type="tel"
         name="number"
         value={number}
-        onChange={this.handleChange}
+        onChange={handleChange}
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
@@ -60,7 +81,7 @@ reset = () => {
             </form>
         )
     }
-} 
+
 
 
 export default ContactForm;
